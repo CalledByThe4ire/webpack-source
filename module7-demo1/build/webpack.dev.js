2f1c2a461b2e349e492732019eb47502a337dcc3
Cstrp <valery.nosareu@gmail.com>
2022-11-29 19:48:15 +0300

    update

--- module7-demo1/build/webpack.dev.js
+++ module7-demo1/build/webpack.dev.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-const defaultUrls = [];
-
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
@@ -19,20 +17,5 @@ module.exports = {
     compress: true,
     port: 4321,
     hot: false,
-    proxy: {
-      context: defaultUrls.map((itUrl) => `/${itUrl}`),
-      '/api': {
-        target: 'http://localhost:4321',
-        pathRewrite: {'^/api': ''},
-      },
-      before(app) {
-        app.use((req, res, next) => {
-          if (req.url.indexOf('/api') === 0 || req.url.indexOf('/static') === 0) {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-          }
-          next();
-        });
-      },
-    },
   },
 };
